fac3abb18115c7aec75c231eb67a86196382354d
unknownzone96 <ivan.a.velev@gmail.com>
2016-05-31 00:58:12 +0300

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -4,6 +4,7 @@ Cell::Cell()
 {
 	this->occupied = false;
 	this->hit = false;
+	this->missed = false;
 }
 
 Cell::~Cell()
@@ -20,6 +21,16 @@ void Cell::set_hit()
 	this->hit = true;
 }
 
+void Cell::clear()
+{
+	this->occupied = false;
+}
+
+void Cell::set_missed()
+{
+	this->missed = true;
+}
+
 bool Cell::get_occupied()
 {
 	return occupied;
@@ -29,3 +40,9 @@ bool Cell::get_hit()
 {
 	return hit;
 }
+
+bool Cell::get_missed()
+{
+	return missed;
+}
+
